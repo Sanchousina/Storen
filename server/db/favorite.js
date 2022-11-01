5b0b597508afcfc72190c23f6aa3c2bea533a416
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-11-01 22:20:20 +0100

    Delete favorite route

--- server/db/favorite.js
+++ server/db/favorite.js
@@ -1,5 +1,19 @@
 import { Connection } from "./index.js";
 
+//TODO:
+// export const all = async(user) => {
+//     return new Promise((resolve, reject) => {
+//         Connection.query(
+//             `SELECT * FROM Favorite`, [advert, user],
+//             (err, results) => {
+//             if(err){
+//                 reject(err);
+//             }
+//             resolve();
+//         });
+//     });
+// }
+
 export const toFavorite = async(advert, user) => {
     return new Promise((resolve, reject) => {
         Connection.query(
@@ -14,6 +28,21 @@ export const toFavorite = async(advert, user) => {
     });
 }
 
+export const unFavorite = async(advert, user) => {
+    return new Promise((resolve, reject) => {
+        Connection.query(
+            `DELETE FROM Favorite
+            WHERE advert_id = ? && user_id = ?`, [advert, user],
+            (err, results) => {
+            if(err){
+                reject(err);
+            }
+            resolve();
+        });
+    });
+}
+
 export default {
-    toFavorite
+    toFavorite,
+    unFavorite
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -3,6 +3,18 @@ import DB from '../db/index.js';
 
 const router = express.Router();
 
+//TODO:
+// router.get('/:user_id/favorite', async (req, res) => {
+//     const user_id = req.params.user_id;
+
+//     try{
+
+//     }catch(err){
+//         console.log(err);
+//         res.sendStatus(500);
+//     }
+// });
+
 router.post('/:user_id/favorite/adverts/:advert_id', async (req, res) => {
     const advert_id = req.params.advert_id;
     const user_id = req.params.user_id;
@@ -10,6 +22,19 @@ router.post('/:user_id/favorite/adverts/:advert_id', async (req, res) => {
     try{
         await DB.favorite.toFavorite(advert_id, user_id);
         res.sendStatus(201);
+    }catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
+
+router.delete('/:user_id/unfavorite/adverts/:advert_id', async (req, res) => {
+    const advert_id = req.params.advert_id;
+    const user_id = req.params.user_id;
+
+    try{
+        await DB.favorite.unFavorite(advert_id, user_id);
+        res.sendStatus(200);
     }catch(err){
         console.log(err);
         res.sendStatus(500);
