11a1e4417fd273e57a5b8752ee93175588ab870e
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-11-10 13:27:20 +0100

    Get images for advert from s3

--- server/db/gallery.js
+++ server/db/gallery.js
@@ -3,7 +3,7 @@ import { Connection } from './index.js'
 export const all = async (id) => {
     return new Promise((resolve, reject) => {
         Connection.query(
-            `SELECT image_url FROM gallery
+            `SELECT image_name FROM gallery
             WHERE advert_id = ?`, [id],
             (err, results) => {
                 if(err){
--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -1,11 +1,26 @@
 import * as express from 'express';
 import DB from '../db/index.js';
+import { s3 } from '../helpers/s3Client.js';
+import { PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import crypto from 'crypto';
 
 const router = express.Router();
 
 router.get('/:advert_id/images', async (req, res) => {
     try{
         let images = await DB.gallery.all(req.params.advert_id);
+
+        for (const image of images){
+            const getObjectParams = {
+                Bucket: process.env.BUCKET_NAME,
+                Key: image.image_name,
+            }
+            const cmd = new GetObjectCommand(getObjectParams);
+            let url = await getSignedUrl(s3, cmd, { expiresIn: 3600 });
+            image.image_url = url;
+        }
+
         res.json(images);
     }catch(err){
         console.log(err);
