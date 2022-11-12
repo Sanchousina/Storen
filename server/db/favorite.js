23bcf8daa0075712a9e09c0a7910231f5371da10
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-11-12 16:36:17 +0100

    Get all user favorites route

--- server/db/favorite.js
+++ server/db/favorite.js
@@ -1,18 +1,25 @@
 import { Connection } from "./index.js";
 
-//TODO:
-// export const all = async(user) => {
-//     return new Promise((resolve, reject) => {
-//         Connection.query(
-//             `SELECT * FROM Favorite`, [advert, user],
-//             (err, results) => {
-//             if(err){
-//                 reject(err);
-//             }
-//             resolve();
-//         });
-//     });
-// }
+export const all = async(user) => {
+    return new Promise((resolve, reject) => {
+        Connection.query(
+            `SELECT advert.advert_id, advert.title, warehouse.city, 
+            warehouse.available_space, warehouse.type, MIN(gallery.image_name) AS image_name
+            FROM advert
+            INNER JOIN warehouse ON advert.advert_id = warehouse.advert_id
+            INNER JOIN gallery ON warehouse.advert_id = gallery.advert_id
+            INNER JOIN favorite ON gallery.advert_id = favorite.advert_id
+            WHERE favorite.user_id = ?
+            GROUP BY advert_id`, 
+            [user],
+            (err, results) => {
+            if(err){
+                reject(err);
+            }
+            resolve(results);
+        });
+    });
+}
 
 export const toFavorite = async(advert, user) => {
     return new Promise((resolve, reject) => {
@@ -43,6 +50,7 @@ export const unFavorite = async(advert, user) => {
 }
 
 export default {
+    all,
     toFavorite,
     unFavorite
 }
--- server/routes/favorite.js
+++ server/routes/favorite.js
@@ -1,24 +1,26 @@
 import * as express from 'express';
 import DB from '../db/index.js';
+import { getS3Url } from '../helpers/s3Client.js';
 
 const router = express.Router();
 
-//TODO:
-// router.get('/:user_id/favorite', async (req, res) => {
-//     const user_id = req.params.user_id;
-
-//     try{
-
-//     }catch(err){
-//         console.log(err);
-//         res.sendStatus(500);
-//     }
-// });
+router.get('/:user_id/favorite', async (req, res) => {
+    const user_id = req.params.user_id;
+    try{
+        const favorites = await DB.favorite.all(user_id);
+        for (const fav of favorites){
+            fav.image_url = await getS3Url(fav.image_name);
+        }
+        res.json(favorites);
+    }catch(err){
+        console.log(err);
+        res.sendStatus(500);
+    }
+});
 
 router.post('/:user_id/favorite/adverts/:advert_id', async (req, res) => {
     const advert_id = req.params.advert_id;
     const user_id = req.params.user_id;
-
     try{
         await DB.favorite.toFavorite(advert_id, user_id);
         res.sendStatus(201);
@@ -31,7 +33,6 @@ router.post('/:user_id/favorite/adverts/:advert_id', async (req, res) => {
 router.delete('/:user_id/unfavorite/adverts/:advert_id', async (req, res) => {
     const advert_id = req.params.advert_id;
     const user_id = req.params.user_id;
-
     try{
         await DB.favorite.unFavorite(advert_id, user_id);
         res.sendStatus(200);
