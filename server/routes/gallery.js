51fb6b8afd39cb153135625e5064e2af661e1824
Sanchousina <ohotnikova.sasha2002@gmail.com>
2022-12-10 14:45:10 +0100

    User permissions for Gallery endpoints

--- server/routes/gallery.js
+++ server/routes/gallery.js
@@ -1,9 +1,9 @@
 import * as express from 'express';
 import DB from '../db/index.js';
 import multer from 'multer';
-import { s3, sendToS3, deleteFromS3, getS3Url, randomName } from '../helpers/s3Client.js';
-import { GetObjectCommand} from "@aws-sdk/client-s3";
-import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
+import { sendToS3, deleteFromS3, getS3Url, randomName } from '../helpers/s3Client.js';
+import { verifyToken } from '../middleware/jwt.js';
+import { ROLES_LIST, verifyAdvertPermissions, verifyRole } from '../middleware/verifyRole.js';
 
 const IMG_FOLDER = 'imgs/';
 
@@ -11,9 +11,9 @@ const router = express.Router();
 const storage = multer.memoryStorage();
 const upload = multer({storage: storage});
 
-router.get('/adverts/:advert_id/images', async (req, res) => {
+router.get('/adverts/:advertId/images', async (req, res) => {
     try{
-        let images = await DB.gallery.all(req.params.advert_id);
+        let images = await DB.gallery.all(req.params.advertId);
         for (const image of images){
             image.image_url = await getS3Url(image.image_name);
         }
@@ -24,14 +24,17 @@ router.get('/adverts/:advert_id/images', async (req, res) => {
     }
 });
 
-router.post('/adverts/:advert_id/images', 
-    upload.array('images', 10),
+router.post('/adverts/:advertId/images',
+    [verifyToken,
+    verifyRole([ROLES_LIST[0], ROLES_LIST[2]]),
+    verifyAdvertPermissions,
+    upload.array('images', 10)],
     async (req, res) => {
         for(const file of req.files){
             const fileName = IMG_FOLDER + randomName();
             sendToS3(file, fileName);
             try{
-                await DB.gallery.insert(req.params.advert_id, fileName);
+                await DB.gallery.insert(req.params.advertId, fileName);
             }catch(err){
                 console.log(err);
                 res.sendStatus(500);
@@ -40,15 +43,19 @@ router.post('/adverts/:advert_id/images',
         res.sendStatus(200);
 });
 
-router.delete('/adverts/:advert_id/images', async(req, res) => {
+router.delete('/adverts/:advertId/images', 
+    [verifyToken,
+    verifyRole([ROLES_LIST[0], ROLES_LIST[2]]),
+    verifyAdvertPermissions],
+    async(req, res) => {
     try{
-        let images = await DB.gallery.all(req.params.advert_id);
+        let images = await DB.gallery.all(req.params.advertId);
         for(const image of images){
             const image_name = image.image_name;
             await deleteFromS3(image_name);
         }
 
-        await DB.gallery.deleteAll(req.params.advert_id);
+        await DB.gallery.deleteAll(req.params.advertId);
         res.sendStatus(200);
     }catch(err){
         console.log(err);
@@ -56,13 +63,18 @@ router.delete('/adverts/:advert_id/images', async(req, res) => {
     }
 });
 
-router.delete('/images/:image_id', async(req, res) => {
+router.delete('/adverts/:advertId/images/:imageId', 
+    [verifyToken,
+    verifyRole([ROLES_LIST[0], ROLES_LIST[2]]),
+    verifyAdvertPermissions],
+    async(req, res) => {
+    const imageId = req.params.imageId;
     try{
-        const image = await DB.gallery.one(req.params.image_id);
+        const image = await DB.gallery.one(imageId);
         const image_name = image.image_name;
         await deleteFromS3(image_name);
 
-        await DB.gallery.deleteOne(req.params.image_id);
+        await DB.gallery.deleteOne(imageId);
         res.sendStatus(200);
     }catch(err){
         console.log(err);
